415e5c1d378be21c4cd9cae9f072fad2b74bd664
Stefan Helmert <stefan.helmert@htw-dresden.de>
2025-09-25 13:47:51 +0200

    frame nearly ready

--- src/mech/wsciot-mountingframe/wsciot_mountingframe/__main__.py
+++ src/mech/wsciot-mountingframe/wsciot_mountingframe/__main__.py
@@ -21,7 +21,7 @@ def f(p):
 
     wo = 70
     lo = 104
-    ho = 25
+    ho = 22
     w_pcb = 47.5
     l_pcb = 80
     y_pcb = -10
@@ -31,25 +31,33 @@ def f(p):
     z_slot = 8
     w_opn = 35
 
-    w_bat = 60
-    l_bat = 18
-    h_bat = 18
+    w_bat = 65 + tol
+    l_bat = 18 + tol
+    h_bat = 18 + tol
 
     x_bat = 0
     y_bat = - l_bat / 2 + lo / 2 - 2
     z_bat = - h_bat / 2 + ho / 2 - 2
     
+    w_opn_bat = wo/2 - (w_pcb/2 + x_pcb) - 2 
+    l_opn_bat = lo - 10
+
+    l_opn_top = lo - 15
+    h_opn_top = 10
 
     solid = bd.fz_cuboid((x, y, z), (wo, lo, ho), 2)
     pcb_slot = bd.fz_cuboid((x+x_pcb, y+y_pcb, z+ho/2-z_slot), (w_pcb+tol, l_pcb+tol, h_slot+tol), r_chamfer)
     pcb = bd.fz_cuboid((x+x_pcb, y+y_pcb, z), (w_pcb-2*d_slot, l_pcb-2*d_slot, 2*ho), r_chamfer)
     corners = bd.fz_circle((abs(x+x_pcb)-w_pcb/2, abs(y+y_pcb)-l_pcb/2), r_corner)
-    opn = bd.fz_cuboid((x+x_pcb, y+l_pcb/2, z), (w_opn, l_pcb-2*d_slot, 2*ho), r_chamfer)
+    opn_pcb = bd.fz_cuboid((x+x_pcb, y-l_pcb/2, z), (w_opn, l_pcb-2*d_slot, 2*ho), r_chamfer)
     bat = bd.fz_cuboid((x+x_bat, y+y_bat, z+z_bat), (w_bat, l_bat, h_bat), r_chamfer)
+    opn_bat = bd.fz_cuboid((x+wo-w_opn_bat, y, z), (wo, l_opn_bat, 2*ho), r_chamfer)
+    opn_top = bd.fz_cuboid((x, y, z-ho+h_opn_top), (2*wo, l_opn_top, ho), r_chamfer)
 
 
 
-    if cmb.fz_and_chamfer(r_chamfer, solid, -pcb_slot, -pcb, -corners, -opn, -bat) > 0:
+
+    if cmb.fz_and_chamfer(r_chamfer, solid, -pcb_slot, -pcb, -corners, -opn_pcb, -bat, -opn_bat, -opn_top) > 0:
         return False
 
     return True
