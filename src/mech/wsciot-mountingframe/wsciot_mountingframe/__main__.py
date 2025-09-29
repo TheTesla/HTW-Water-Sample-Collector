7d000c9392cb02687f4c551d95a7be14fd9ec401
Stefan Helmert <stefan.helmert@htw-dresden.de>
2025-09-29 19:34:30 +0200

    antenna area, battery clip fits better

--- src/mech/wsciot-mountingframe/wsciot_mountingframe/__main__.py
+++ src/mech/wsciot-mountingframe/wsciot_mountingframe/__main__.py
@@ -30,30 +30,31 @@ def f(p):
     ri_pcb = 3
     
     h_slot = 1.6
-    z_slot = 8
+    z_slot = 11
     w_opn = 35
 
     w_bat = 60 #+ 2*tol
     l_bat = 15.5 #+ 2*tol
-    h_bat = 17 #+ 2*tol
+    h_bat = 18 #+ 2*tol
 
     w_bat_cut = wo - 7
     l_bat_cut = 9
     ri_bat_cut = 3
 
-    x_bat = -1
-    y_bat = - l_bat / 2 + lo / 2 - 1
+    x_bat = - 1
+    y_bat = - l_bat / 2 + lo / 2 - 1.5
     z_bat = - h_bat / 2 + ho / 2 - 2
     
 
 
-    w_opn_bat = wo/2 - (w_pcb/2 + x_pcb) - 2 
-    l_opn_bat = lo - 23
-    y_opn_bat = -7.5
-    ri_opn_bat = 3
+    w_opn_ant = wo/2 - (w_pcb/2 + x_pcb) - 2 
+    l_opn_ant = lo - 10
+    y_opn_ant = -1
+    z_opn_ant = -2
+    ri_opn_ant = 3
 
     l_opn_top = lo - 30
-    h_opn_top = 10
+    h_opn_top = 7
     y_opn_top = -8
     ri_opn_top = 3
 
@@ -64,13 +65,13 @@ def f(p):
     opn_pcb = bd.fz_cuboid((x+x_pcb, y-l_pcb/2, z), (w_opn, l_pcb-2*d_slot, 2*ho), r_chamfer)
     bat = bd.fz_cuboid((x+x_bat, y+y_bat, z+z_bat), (w_bat, l_bat, h_bat), r_chamfer)
     bat_cut = bd.fz_cuboid((x+wo/2, y+y_bat, z), (2*w_bat_cut, l_bat_cut, 2*ho), ri_bat_cut)
-    opn_bat = bd.fz_cuboid((x+wo-w_opn_bat, y+y_opn_bat, z), (wo, l_opn_bat, 2*ho), ri_opn_bat)
+    opn_ant = bd.fz_cuboid((x+wo-w_opn_ant, y+y_opn_ant, z+z_opn_ant-ho/2), (wo, l_opn_ant, 2*ho), ri_opn_ant)
     opn_top = bd.fz_cuboid((x, y+y_opn_top, z-ho+h_opn_top), (2*wo, l_opn_top, ho), ri_opn_top)
 
 
 
 
-    if cmb.fz_and_chamfer(r_chamfer, solid, -pcb_slot, -pcb, -corners, -opn_pcb, -bat, -opn_bat, -opn_top, -bat_cut) > 0:
+    if cmb.fz_and_chamfer(r_chamfer, solid, -pcb_slot, -pcb, -corners, -opn_pcb, -bat, -opn_ant, -opn_top, -bat_cut) > 0:
         return False
 
     return True
